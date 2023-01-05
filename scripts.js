f19e223af456868d0097c3eaeed72338264b3408
Julian Battaglino <julianbattaglino@gmail.com>
2023-01-05 15:41:38 -0300

    updates

--- scripts.js
+++ scripts.js
@@ -109,6 +109,42 @@ $(document).ready(function () {
 		cssEase: 'linear'
 	});
 
+	$('.slick-slider-gallery').slick({
+		dots: true,
+		speed: 300,
+		slidesToShow: 4,
+		centerMode: false,
+		focusOnSelect: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		responsive: [
+		  {
+			breakpoint: 1200,
+			settings: {
+			  slidesToShow: 3,
+			  infinite: true,
+			  dots: true
+			}
+		  },
+		  {
+			breakpoint: 1024,
+			settings: {
+			  slidesToShow: 2,
+			}
+		  },
+		  {
+			breakpoint: 767,
+			settings: {
+			  slidesToShow: 1,
+			}
+		  }
+		  // You can unslick at a given breakpoint now by adding:
+		  // settings: "unslick"
+		  // instead of a settings object
+		]
+		
+	  });    
+
 	// Isotope Filters Projects Page
 	$('.isotope-grid').isotope({
 		itemSelector: '.grid-item',
